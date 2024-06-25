89e3e492efe0c88c1e6a496a40e3459c7eb54d7d
hansdf <dornsback@gmail.com>
2024-06-25 11:25:11 -0300

    roadmap for the project!

--- tasks_manager.py
+++ tasks_manager.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+# roadmap for this project!
+
+# create a main class to act as menu / body, showing all the tasks
+# create methods to add, delete, and update(done/changed) tasks (read about CRUD)
+
+# implement some sort of db system to save the tasks list when finished
+    # experiment with JSON?
+
+# additional functionality with more advanced methods, such as search or filter specific tasks
