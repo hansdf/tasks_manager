512103bc7d45d051c96fef6aed7f5d2a9aa7695b
hansdf <dornsback@gmail.com>
2024-06-25 13:47:07 -0300

    first tasks body implementation

--- tasks_manager.py
+++ tasks_manager.py
@@ -1,4 +1,5 @@
-print("hello world")
+import os, sys
+from datetime import date
 
 # roadmap for this project!
 
@@ -9,3 +10,24 @@ print("hello world")
     # experiment with JSON?
 
 # additional functionality with more advanced methods, such as search or filter specific tasks
+
+def main():
+    
+    while True:
+        show_menu()
+        keypress = input("press q to exit ")
+        if keypress == "q":
+            sys.exit()
+
+def show_menu():
+    today = date.today()
+    print("Hans's tasks :)")
+    print(f"Current date is {today}")
+    print("Task 1...")
+    print("Task 2...")
+    print("Task 3...")
+    print("Task 4...")
+    print("Task 5...")
+
+if __name__ == "__main__":
+    main()
